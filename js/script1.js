7d68475aef8b48cc059e8f304bd1327c4af65318
Henrique <henriquemiranda47@gmail.com>
2020-06-29 16:39:38 -0300

    Basic validation added

---
+++ js/script1.js
@@ -0,0 +1,14 @@
+function validateForm(){
+    var name = document.forms["clientForm"]["clientName"].value;
+    var surName = document.forms["clientForm"]["clientSurname"].value;
+    var gender = document.forms["clientForm"]["clientGender"].value;
+    var cellPhone = document.forms["clientForm"]["clientCellPhone"].value;
+    var email = document.forms["clientForm"]["clientEmail"].value;
+    alert("Name: " + name + "\n" +
+          "Surname: " + surName + "\n" +
+          "Gender: " + gender + "\n" +
+          "Cellphone: " + cellPhone + "\n" +
+          "E-mail: " + email + "\n" 
+    );
+    
+}
