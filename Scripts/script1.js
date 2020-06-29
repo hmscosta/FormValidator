1bc869bf7e57e0a5c0fcb4d379708863e5440d2b
Henrique <henriquemiranda47@gmail.com>
2020-06-29 15:07:20 -0300

    Initial version

---
+++ Scripts/script1.js
@@ -0,0 +1,16 @@
+
+const constante_1 = 'Sou uma constante'
+let variavel_1 = 'Sou uma variavel';
+console.log('Teste');
+console.log(constante_1);
+console.log(variavel_1);
+
+console.log(somar('2','5'));
+
+
+
+
+
+function somar(num1,num2){
+    return num1 + num2;
+}
